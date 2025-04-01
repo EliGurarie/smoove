9d906d385dba0cde9ff6ca63df15b0bb9fbdcfe7
Eliezer Gurarie <egurarie@esf.edu>
2025-04-01 16:58:36 -0400

    Modified plotPhaseParameter function as per flagged issue

--- R/plotPhaseParameter.R
+++ R/plotPhaseParameter.R
@@ -7,7 +7,7 @@
 #' @param phaselist output of \code{\link{estimatePhases}}, i.e. a list of information on each of the selected phases
 #' @param cols colors of bars (by default - a rich color palette)
 #' @param label whether to label the plotted parameter (with the word "tau", "eta", etc.)
-#' @param ymax maximum y
+#' @param log a character string which contains "y" if the y axis is to be logarithmic and "xy" or "yx" if both axes are to be logarithmic.
 #' @param ... additional arguments to pass to plotting function.  A useful one is \code{log = "y"} for highly skewed data.
 #' @return \code{getVariable} returns a data.frame with start time, end time, estimate, low and high 95\% C.I. and selected model for a given parameter, with each row corresponding to an enumerated partition phase.  
 #' @examples 
@@ -48,19 +48,40 @@
 #' 
 #' @export
 #' 
-plotPhaseParameter <- function(variable, phaselist, cols = 1:length(phaselist), 
-                               label = TRUE, ...){
+plotPhaseParameter <- function(variable, phaselist, 
+                               cols = 1:length(phaselist), 
+                               label = TRUE, log = "", ...){
   variabletable <- getPhaseParameter(variable, phaselist)
   
   low.plot <- variabletable$low
   high.plot <- variabletable$high
-  #high.plot[high.plot > ymax] <- ymax
+
+  if(any(low.plot == -Inf)){
+    which.inf <- which(low.plot == -Inf)
+    low.plot[low.plot == -Inf] <- 10*min(low.plot[low.plot != -Inf])
+    warning(cat("The lower confidence limit for phase", as.roman(which.inf), 
+                "is negative infinity and will be plotted as very negative. Consider setting y limits manually.\n"))
+  }
+  
+  if(any(low.plot == 0) & log == "y"){
+    which.zero <- which(low.plot == 0)
+    low.plot[low.plot == 0] <- .0001
+    warning(cat("The lower confidence limit for phase", as.roman(which.zero), 
+                "is 0, which will be plotted on the log-scale as something very negative. Consider setting y limits manually.\n"))
+  }
+  
+  if(any(high.plot == Inf)){
+    which.inf <- which(high.plot == Inf)
+    high.plot[high.plot == Inf] <- 10*max(high.plot[high.plot != Inf])
+    warning(cat("The upper confidence limit for phase", as.roman(which.inf), "is infinity and will be plotted as very positive. Consider setting y limits manually.\n"))
+  }
   
   with(variabletable,{
     plot(range(start, end), 
          range(low.plot, high.plot, na.rm=TRUE), 
-         type="n", ...)
-    if(!is.na(low[1])) rect(start, low.plot, end, high.plot, col = alpha(cols, .5), bor=NA) 
+         type="n", log = log, ...)
+    if(!is.na(low[1])) rect(start, low.plot, end, high.plot, 
+                            col = scales::alpha(cols, .5), bor=NA) 
     
     segments(start, hat, end, hat, lwd = 2, col=cols)
     segments(end[-length(end)], hat[-length(end)], start[-1], hat[-1], col="grey")
